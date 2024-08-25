b184eb85cbdd68b2ac35acd7d1f8dce79f57e312
Bharat Gayki <bhaaratgayki@gmail.com>
2024-08-25 20:13:18 +0530

    MinorChanges

--- Ecommerce-Web-Spring-Boot-Project/src/main/java/com/ecommerce/library/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.library.repo;
-
-import com.ecommerce.library.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    Role findByName(String name);
-}
